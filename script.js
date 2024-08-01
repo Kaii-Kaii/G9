3071c57d147f8c03ac55d610b27185c97a9715c2
Đặng Quốc Khang <128433740+Kaii-Kaii@users.noreply.github.com>
2024-08-01 22:29:18 +0700

    fix

--- script.js
+++ script.js
@@ -1,165 +1,83 @@
-//const loading = document.querySelector('#loader')
+var html = document.getElementsByTagName("html")[0];
+var sun = document.querySelector(".sun");
+var rainbow = document.querySelector(".rainbow");
+var rabbit = document.querySelector(".rabbit");
+var snow = document.querySelectorAll(".snow");
+var rain = document.querySelectorAll(".rain");
+var flowers = document.querySelectorAll(".flower");
+var lightColours = ["#93d5eb", "#add63a", "#c5d63a", "#febe42"],
+mediumColours = ["#66bbd8", "#92c938", "#acc52b", "#ff9d25"],
+darkColours = ["#4da2bb", "#2a9d5c", "#89a503", "#ff6b2f"],
+backgroundColours = ["#cbe9f4", "#daf8ff", "#feec98", "#ffdc8a"],
+bushColours = ["#ffffff", "#3ebf6d", "#99b31a", "#fd6d2e"],
+cloudColours = ["#ffffff", "#ffffff", "#ffffff", "#eaf9fe"],
+seasons = ["Winter", "Spring", "Summer", "Autumn"];
+var c = 1;
+var season;
 
-const canvas = document.querySelector('.webgl')
-const scene = new THREE.Scene()
-const textureLoader = new THREE.TextureLoader()
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+function updateSeasons() {
+  html.style.setProperty("--bgd-color", backgroundColours[c]);
+  html.style.setProperty("--light", lightColours[c]);
+  html.style.setProperty("--medium", mediumColours[c]);
+  html.style.setProperty("--dark", darkColours[c]);
+  html.style.setProperty("--bush", bushColours[c]);
+  html.style.setProperty("--cloud", cloudColours[c]);
+  season = seasons[c];
+
+  //add snow if season = winter
+  snow.forEach(function (el) {
+    season !== seasons[0] ?
+    el.style.display = "none" :
+    el.style.display = "block";
+  });
+
+  //add rabbit if season = winter
+  if (season === seasons[0] || season === seasons[2]) {
+    rabbit.classList.add("animated");
+  } else {
+    rabbit.classList.remove("animated");
+  }
+
+  //add rainbow if season = spring
+  season === seasons[1] ?
+  rainbow.classList.add("animated") :
+  rainbow.classList.remove("animated");
+  if (season === seasons[1]) {
+    html.style.setProperty("--rabbit", "#9E6255");
+  }
+  //add flowers if season = spring
+  flowers.forEach(function (el) {
+    season === seasons[1] ?
+    el.classList.add("animated") :
+    el.classList.remove("animated");
+  });
+
+  //add sun and rabbit if season = summer
+  if (season === seasons[2]) {
+    html.style.setProperty("--sun", "#ffb53a");
+    html.style.setProperty("--rabbit", "#9E6255");
+  } else {
+    html.style.setProperty("--sun", "transparent");
+    html.style.setProperty("--rabbit", "#ffffff");
+  }
+
+  //add rain if season = autumn
+  rain.forEach(function (el) {
+    season === seasons[3] ?
+    el.style.display = "block" :
+    el.style.display = "none";
+  });
+
+  //increment array index
+  c = (c + 1) % seasons.length;
 }
 
-// Base camera
-const camera = new THREE.PerspectiveCamera(15, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = -14
-camera.position.y = 6  
-camera.position.z = 16
-scene.add(camera)
-
-//Controls
-const controls = new THREE.OrbitControls(camera, canvas)
-controls.enableDamping = true
-controls.enableZoom = true
-controls.enablePan = true
-controls.minDistance = 10
-controls.maxDistance = 22
-// controls.maxDistance = 35
-
-controls.minPolarAngle = Math.PI / 5
-controls.maxPolarAngle = Math.PI / 2
-
-// Renderer
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
-    alpha: true
-})
-
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.outputEncoding = THREE.sRGBEncoding
-
-
-// Top
-const light = new THREE.DirectionalLight( '#ffffff', 3 )
-scene.add( light )
-light.position.set(0,3,0)
-light.intensity = 1.5
-
-// // Left
-const light2 = new THREE.DirectionalLight( '#ffffff', 3 )
-scene.add( light2 )
-light2.position.set(-4,0,-2)
-light2.intensity = 1.5
-
-// //Right
-const light3 = new THREE.DirectionalLight( '#ffffff', 3 )
-scene.add( light3 )
-light3.position.set(4,1,2)
-light3.intensity = .5
-
-// // Front
-const light4 = new THREE.DirectionalLight( '#ffffff', 3 )
-scene.add( light4 )
-light4.position.set(0,0,3)
-light4.intensity = .6
-
-// Back
-const light5 = new THREE.DirectionalLight( '#ffffff', 4 )
-scene.add( light5 )
-light5.position.set(0,0,-5)
-light5.intensity = 1
-
-const rectLight = new THREE.RectAreaLight( 0xffffff, .9, 10, 10 )
-rectLight.position.set( 0, 2, 0 )
-rectLight.lookAt( 0, 0, 0 )
-scene.add( rectLight )
-
-// Materials
-const bakedTexture = textureLoader.load('https://rawcdn.githack.com/ricardoolivaalonso/ThreeJS-Room12/cecbd1c77333b3c9ee23bb1eb41dee395e14ca3e/dist/baked.jpg')
-bakedTexture.flipY = false
-bakedTexture.encoding = THREE.sRGBEncoding
-
-const normalTexture = textureLoader.load('https://rawcdn.githack.com/ricardoolivaalonso/ThreeJS-Room12/cecbd1c77333b3c9ee23bb1eb41dee395e14ca3e/dist/normal.jpg')
-normalTexture.flipY = false
-normalTexture.encoding = THREE.sRGBEncoding
-
-const occlusionTexture = textureLoader.load('https://rawcdn.githack.com/ricardoolivaalonso/ThreeJS-Room12/cecbd1c77333b3c9ee23bb1eb41dee395e14ca3e/dist/occlusion.jpg')
-occlusionTexture.flipY = false
-occlusionTexture.encoding = THREE.sRGBEncoding
-
-const bakedMaterial = new THREE.MeshStandardMaterial({ 
-	map: bakedTexture,
-	normalMap: normalTexture,
-    aoMap: occlusionTexture,
-    side: THREE.DoubleSide,
-})
-
-const metalicMaterial = new THREE.MeshStandardMaterial({ 
-    map: bakedTexture,
-    side: THREE.DoubleSide,
-})
-
-//Loader
-const loader = new THREE.GLTFLoader()
-
-loader.load('https://rawcdn.githack.com/ricardoolivaalonso/ThreeJS-Room12/cecbd1c77333b3c9ee23bb1eb41dee395e14ca3e/dist/model.glb',
-    (gltf) => {
-        const model = gltf.scene
-        model.traverse( child => {
-			child.material = metalicMaterial 
-            child.material.roughness = 0
-		})
-        scene.add(model)
-	    //loading.style.display = 'none'
-    },
-	( xhr ) => {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' )
-    }
-)
-
-loader.load('https://rawcdn.githack.com/ricardoolivaalonso/ThreeJS-Room12/cecbd1c77333b3c9ee23bb1eb41dee395e14ca3e/dist/metalic.glb',
-    (gltf) => {
-        const model = gltf.scene
-        model.traverse( child => {
-			child.material = bakedMaterial 
-            child.material.metalness = .85
-            child.material.roughness = .5
-		})
-        scene.add(model)
-    },
-	( xhr ) => {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' )
-    }
-)
-
-
-window.addEventListener('resize', () =>
-{
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-// Animation
-
-let rmapped = 0
-let minPan = new THREE.Vector3( -2, -.5, -2 )
-let maxPan = new THREE.Vector3( 2, .5, 2 )
-
-const tick = () => {
-    controls.update()
-	controls.target.clamp( minPan, maxPan )
-	controls.target.clamp( minPan, maxPan )
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(tick)
-	
-	let h = rmapped * 0.0025 % 1
-    let s = 0.5
-    let l = 0.5
-    // rectLight.color.setHSL ( h, s, l )
-    // rmapped ++
+//animate all
+let nF = 0;
+function animate() {
+  if (++nF % 600 === 0) {
+    updateSeasons();
+  }
+  requestAnimationFrame(animate);
 }
-
-tick()
+animate();
